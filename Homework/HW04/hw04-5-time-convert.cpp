8d632ed0986768afc880cce30aa3fa0c2f284d0d
Mekha1104 <gtrcooie@gmail.com>
2023-08-11 23:15:35 +0700

    04-5

--- Homework/HW04/hw04-5-time-convert.cpp
+++ Homework/HW04/hw04-5-time-convert.cpp
@@ -13,3 +13,23 @@
     Output:
         27 days = 2332800 seconds
 */
+
+#include <stdio.h>
+
+int main() {
+    
+    float days ;
+
+    //รับค่าจำนวนวันจากผู้ใช้
+    printf( "Input Days : " ) ;
+    scanf( "%f",&days ) ;
+
+    //เเปลงจากหน่วย วัน เป็น วินาที
+    float seconds_per_days = 24 * 60 * 60 ;
+    float seconds = days * seconds_per_days ;
+
+    // เเสดงผลลัพธิ์
+    printf("%0.1f days = %0.1f * seconds\n", days , seconds ) ;
+
+    return 0 ;
+}
